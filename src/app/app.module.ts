34cae4e630411df896a36f9c2203b0dbb664bd26
Cao Huy Thong <thongtit1612@gmail.com>
2022-05-04 10:12:28 +0700

    create basic layout and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    ProductsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
